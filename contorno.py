3f7015da4afadf077193bd43098b20b9ba3661ae
vinihsilv <vinihsilv657@gmail.com>
2025-04-12 12:50:13 -0300

    Add image processing script with morphological operations

---
+++ contorno.py
@@ -0,0 +1,5 @@
+import math
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
---
+++ espacoCores.py
@@ -0,0 +1,31 @@
+import os
+import cv2
+import matplotlib.pyplot as plt
+
+dir = './images/'
+# Carregans imagem
+for arquivo in os.listdir(dir):
+    if arquivo.endswith('.jpeg'):
+        img = cv2.imread(dir + arquivo)
+        
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        img_hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+
+        
+        imagens = [img_rgb,img_gray,img_hsv,img_hls]
+
+        for i in range(4):
+            plt.subplot(2,2,i+1)
+            plt.imshow(imagens[i],'gray')
+            plt.xticks([]),plt.yticks([])
+        plt.show()
+
+
+
+
+        plt.show()
+
+
+
